3d4186f34ee5fd8828d9f514541165607472a631
yusuShin <tlsdbtn0507@naver.com>
2025-08-21 18:52:34 +0900

    배포 에러 해결, eslint / export 이슈5

--- src/app/family/add/page.tsx
+++ src/app/family/add/page.tsx
@@ -1,10 +1,6 @@
 // src/app/family/add/page.tsx
-import dynamic from "next/dynamic";
+import FamilyCreateWizard from "@/components/family/familyCreateWiz";
 
-const FamilyCreateWizard = dynamic(
-  () => import("@/components/family/familyCreateWiz"),
-  { ssr: false }                       // ✅ 서버 프리렌더/SSR 금지
-);
 
 export default function Page() {
   return <FamilyCreateWizard />;
